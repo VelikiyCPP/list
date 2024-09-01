397968c0f02a87e7e972ca0914c0918f2d8d8a65
VelikiyCPP <167678849+VelikiyCPP@users.noreply.github.com>
2024-09-02 01:46:42 +0300

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -15,7 +15,6 @@ using is_random_access_iterator = std::is_base_of<std::random_access_iterator_ta
 
 template< class T>
 class List {
-
     using value_type = T;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
@@ -34,17 +33,17 @@ class List {
     struct Node final : public Base_node {
         value_type value{};
 
+        Node() = delete;
+
         Node(const Node&) = delete;
         Node(Node&) = delete;
 
         Node& operator=(Node&) = delete;
         Node& operator=(const Node&) = delete;
 
-        Node() = delete;
+        explicit Node(const_reference value) : value(value) {}
 
-        Node(const_reference value) : value(value) {}
-
-        virtual ~Node() = default;
+        ~Node()override = default;
     };
 
     Base_node base_node_;
@@ -56,21 +55,18 @@ class List {
         using iterator_category = std::bidirectional_iterator_tag;
         using value_type = T;
         using difference_type = std::ptrdiff_t;
-        using pointer = typename std::conditional_t<is_const, const Node*, Node*>;
-        using reference = typename std::conditional_t<is_const, const T&, T&>;
+        using pointer = std::conditional_t<is_const, const Node*, Node*>;
+        using reference = std::conditional_t<is_const, const T&, T&>;
 
         pointer ptr_ = nullptr;
 
-        base_iterator() : ptr_(nullptr) {}
+        base_iterator() = default;
 
-        base_iterator(pointer ptr) : ptr_(ptr) {}
+        explicit base_iterator(pointer ptr) : ptr_(ptr) {}
 
         base_iterator(const base_iterator& other) : ptr_(other.ptr_) {}
 
-        base_iterator& operator=(const base_iterator& other) {
-            ptr_ = other.ptr_;
-            return *this;
-        }
+        base_iterator& operator=(const base_iterator& other) = default;
 
         reference operator*() const {
             return ptr_->value;
@@ -116,6 +112,7 @@ class List {
 
         ~base_iterator() = default;
     };
+
 public:
     using iterator = base_iterator<false>;
     using const_iterator = base_iterator<true>;
@@ -140,9 +137,15 @@ public:
         }
     }
 
-    explicit List(const List& other) : List() {
+    List(const List& other) : List() {
         for (const auto& elem : other) {
             push_back(elem);
+        }
+    }
+
+    List(std::initializer_list<value_type> init_list) : List() {
+        for (const auto& value : init_list) {
+            push_back(value);
         }
     }
 
@@ -224,17 +227,17 @@ public:
     }
 
     // Capacity
-    bool empty() const
+    [[nodiscard]] bool empty() const
     {
         return size_ == 0;
     }
 
-    size_type size() const
+    [[nodiscard]] size_type size() const
     {
         return size_;
     }
 
-    void resize(size_type count)
+    void resize(const size_type count)
     {
         if (count < size_) {
             while (size_ > count) {
@@ -267,7 +270,7 @@ public:
         return iterator(static_cast<Node*>(base_node_.next));
     }
 
-    const_iterator cbegin() const {
+    [[nodiscard]] const_iterator cbegin() const {
         return const_iterator(static_cast<const Node*>(base_node_.next));
     }
 
@@ -275,7 +278,7 @@ public:
         return iterator(static_cast<Node*>(&base_node_));
     }
 
-    const_iterator cend() const {
+    [[nodiscard]] const_iterator cend() const {
         return const_iterator(static_cast<const Node*>(&base_node_));
     }
 
@@ -283,7 +286,7 @@ public:
         return reverse_iterator(end());
     }
 
-    const_reverse_iterator crbegin() const {
+    [[nodiscard]] const_reverse_iterator crbegin() const {
         return const_reverse_iterator(cend());
     }
 
@@ -291,7 +294,7 @@ public:
         return reverse_iterator(begin());
     }
 
-    const_reverse_iterator crend() const {
+    [[nodiscard]] const_reverse_iterator crend() const {
         return const_reverse_iterator(cbegin());
     }
 
@@ -299,13 +302,13 @@ public:
     reference front() {
         return static_cast<Node*>(base_node_.next)->value;
     }
-    const_reference front() const {
+    [[nodiscard]] const_reference front() const {
         return static_cast<Node*>(base_node_.next)->value;
     }
     reference back() {
         return static_cast<Node*>(base_node_.prev)->value;
     }
-    const_reference back() const {
+    [[nodiscard]] const_reference back() const {
         return static_cast<Node*>(base_node_.prev)->value;
     }
 
@@ -315,106 +318,117 @@ public:
 
         new_node->next = current_node;
         new_node->prev = current_node->prev;
-        current_node->prev->next = new_node; 
-        current_node->prev = new_node;           ++size_;
-        
+        current_node->prev->next = new_node;
+        current_node->prev = new_node;
+
+        ++size_;
+
         return iterator(new_node);
     }
 
     iterator insert(const_iterator pos, size_type count, const_reference value) { // test
         iterator iter(const_cast<Node*>(pos.ptr_));
-        
-        for (size_type i = 0; i < count; ++i) {
+
+        for (size_type i(0); i < count; ++i) {
             iter = insert(iter, value);
         }
-        
+
         return iter;
     }
 
     template<std::input_iterator InputIt>
     iterator insert(const_iterator pos, InputIt first, InputIt last) {
-        iterator ret_iter(const_cast<Node*>(pos.ptr_));
-        
-        for (auto it = std::prev(last); it != std::prev(first); --it) {
-            ret_iter = insert(ret_iter, *it);
+        iterator insert_pos(const_cast<Node*>(pos.ptr_));
+
+        std::reverse_iterator<InputIt> rev_first(last);
+        std::reverse_iterator<InputIt> rev_last(first);
+
+        for (auto it = rev_first; it != rev_last; ++it) {
+            insert_pos = insert(insert_pos, *it);
         }
-        return ret_iter;
+
+        return insert_pos;
     }
+
 
     iterator erase(const_iterator pos) {
         if (pos.ptr_ == &base_node_) {
             throw std::logic_error("Cannot erase from the base node");
-        } else if (empty()) {
+        }
+
+        if (empty()) {
             throw std::out_of_range("Cannot erase from an empty list.");
         }
-        
+
         Node* current_node = const_cast<Node*>(pos.ptr_);
 
         current_node->prev->next = current_node->next;
         current_node->next->prev = current_node->prev;
-        
+
         Node* next_node = static_cast<Node*>(current_node->next);
-    
+
         delete current_node;                             --size_;
-    
+
         return iterator(next_node);
     }
 
     iterator erase(const_iterator first, const_iterator last) {
-        
+
         if (first.ptr_ == &base_node_ || last.ptr_ == &base_node_) {
             throw std::logic_error("Cannot erase from the base node");
-        } else if (first == last) {
+        }
+
+        if (first == last) {
             return iterator(const_cast<Node*>(last.ptr_));
         }
-    
+
         Node* first_node = const_cast<Node*>(first.ptr_);
         Node* last_node = const_cast<Node*>(last.ptr_);
-    
+
         first_node->prev->next = last_node;
         last_node->prev = first_node->prev;
-        
+
         while (first_node != last_node) {
             Node* next_node = static_cast<Node*>(first_node->next);
-            
+
             delete first_node;
-            
+
             first_node = next_node;    --size_;
         }
-        
+
         return iterator(last_node);
     }
 
-    void clear() {
+    void clear() noexcept {
         if (base_node_.next == &base_node_) {
             return;
         }
-    
+
         Node* current = static_cast<Node*>(base_node_.next);
-    
+
         while (current != &base_node_) {
             Node* next_node = static_cast<Node*>(current->next);
-            
+
             current->~Node();
             delete current;
-            
+
             current = next_node;
         }
-    
+
         base_node_.next = &base_node_;
         base_node_.prev = &base_node_;
-        
+
         size_ = 0;
     }
 
-    void sort() {
+    void sort() noexcept {
         if (size_ > 1){
             bool swapped;
-            
+
             do {
                 swapped = false;
-                for (auto it = begin(); std::next(it) != end(); ++it) {
-                    auto next_it = std::next(it);
+                for (iterator it = begin(); std::next(it) != end(); ++it) {
+                    iterator next_it = std::next(it);
                     if (*next_it < *it) {
                         std::iter_swap(it, next_it);
                         swapped = true;
@@ -424,58 +438,69 @@ public:
         }
     }
 
-    void swap(List& other) {
-        using std::swap;
-        swap(base_node_, other.base_node_);
-        swap(size_, other.size_);
+    void swap(List& other) noexcept {
+        std::swap(base_node_, other.base_node_);
+        std::swap(size_, other.size_);
     }
 
     void unique() {
         if (size_ > 1) {
-            for (auto it = begin(), next_it = std::next(it); next_it != end(); ) {
+            for (auto it = begin(), next_it = std::next(it); next_it != end();) {
                 if (*it == *next_it) {
-                    next_it = erase(next_it);
+                    next_it(erase(next_it));
                     continue;
                 }
-                
-                (++it,++next_it);
+
+                ++next_it; ++it;
             }
         }
     }
 
-    // Please note: This code was generated using artificial intelligence without testing.
     void merge(List& other) {
-        auto it1 = begin();
-        auto it2 = other.begin();
-
-        while (it2 != other.end()) {
-            if (it1 == end() || *it2 < *it1) {
-                insert(it1, *it2);
-                it2 = other.erase(it2);
-            }
-            else {
-                ++it1;
-            }
+        if (other.empty()) {
+            return;
         }
+
+        if (empty()) {
+            base_node_.next = other.base_node_.next;
+            base_node_.prev = other.base_node_.prev;
+
+            other.base_node_.next->prev = &base_node_;
+            other.base_node_.prev->next = &base_node_;
+
+            other.base_node_.next = &other.base_node_;
+            other.base_node_.prev = &other.base_node_;
+
+            return;
+        }
+
+        base_node_.prev->next = other.base_node_.next;
+        other.base_node_.next->prev = base_node_.prev;
+
+        other.base_node_.prev->next = &base_node_;
+        base_node_.prev = other.base_node_.prev;
+
+        other.base_node_.next = &other.base_node_;
+        other.base_node_.prev = &other.base_node_;
+
+        sort();
     }
 
     void reverse() noexcept {
-        
-        iterator left = begin();
+
+        iterator left  = begin();
         iterator right = std::prev(end());
-        
+
         while (left != right && left.ptr_ != right.ptr_->next) {
             std::iter_swap(left, right);
-            ++left;
-            --right;
+            ++left; --right;
         }
     }
-    
+
     void assign(size_type count, const_reference value) {
         clear();
 
         if (count != 0) {
-
             Node* current = new Node(value);
 
             base_node_.next = current;
@@ -493,65 +518,18 @@ public:
             size_ = count;
         }
     }
-    
+
     template <std::input_iterator InputIt>
     void assign(InputIt first, InputIt last) {
         clear();
-    
+
         for (auto it = first; it != last; ++it) {
             push_back(*it);
         }
     }
 
-    // Please note: This code was generated using artificial intelligence without testing.
-    void splice(const_iterator pos, List& other) {
-        if (this == &other || other.empty()) return;
-
-        auto first = other.begin();
-        auto last = other.end();
-        splice(pos, other, first, last);
-    }
-
-    // Please note: This code was generated using artificial intelligence without testing.
-    void splice(const_iterator pos, List& other, const_iterator it) {
-        auto next_it = std::next(it);
-        splice(pos, other, it, next_it);
-    }
-
-    // Please note: This code was generated using artificial intelligence without testing.
-    void splice(const_iterator pos, List& other, const_iterator first, const_iterator last) {
-        if (first == last) return;
-
-        auto prev_first = first.ptr_->prev;
-        auto next_last = last.ptr_;
-
-        prev_first->next = next_last;
-        next_last->prev = prev_first;
-
-        first.ptr_->prev = const_cast<Base_node*>(pos.ptr_->prev);
-        last.ptr_->prev->next = const_cast<Base_node*>(pos.ptr_);
-        pos.ptr_->prev->next = first.ptr_;
-        pos.ptr_->prev = last.ptr_->prev;
-
-        size_ += std::distance(first, last);
-        other.size_ -= std::distance(first, last);
-    }
-
-    // Please note: This code was generated using artificial intelligence without testing.
-    void remove(const_reference value) {
-        auto it = begin();
-        while (it != end()) {
-            if (*it == value) {
-                it = erase(it);
-            }
-            else {
-                ++it;
-            }
-        }
-    }
-
     ~List() {
-        clear(); 
+        clear();
     }
 };
 
@@ -560,30 +538,11 @@ public:
 
 int main() {
     std::vector<int> vec{};
-    List<int> list;
 
-    list.insert(list.cbegin(), vec.begin(), vec.end());
+    List<int> list{1,2,6,77,6,1,2,3,54,34,54,2,3,1,3,4,5,19,10};
+    List<int> list2{6,7,8,9,10};
 
-    //list.erase(list.begin());
-
-    list.push_back(100);
-    list.push_front(500);
-
-    list.assign(vec.begin(), vec.end());
-    
-    //list.sort();
-    
-    list.reverse();
-    list.push_back(100);
-    list.push_back(500);
-    list.push_back(500);
-    list.push_back(500);
-    list.push_back(500);
-    list.push_back(100);
-    list.push_back(100);
-    list.push_back(100);
-    list.reverse();
-    list.push_back(500);
+    list.merge(list2);
     list.unique();
 
     for(const auto& i : list){
